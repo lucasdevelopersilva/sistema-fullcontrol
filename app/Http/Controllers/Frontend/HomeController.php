ce81da71692116bfe0aa75ff57ecc80599a592e9
lwagenciaweb <lwagenciaweb@gmail.com>
2021-10-17 00:00:32 -0300

    Criado os controllers do sistema backend

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        echo 'php';
+//        return view("frontend.index");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\Frontend\BlogController;
+use App\Http\Controllers\Frontend\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::namespace("Frontend")->group(function(){
+    Route::get("/", [HomeController::class,"index"]); 
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace("Backend")->group(function(){
+    Route::get("/dashboard",[DashController::class,"index"]);
+    //noticias
+    Route::get("/noticias",[PostController::class,"index"]);
+    Route::get("/addnoticia",[PostController::class,"edit"]);
+    Route::post("/addnoticia",[PostController::class,"save"]);
+    //programação
+    Route::get("/programacao",[ProgramaController::class,"index"]);
+    Route::get("/addprogramacao",[ProgramaController::class,"edit"]);
+    Route::post("/addprogramacao",[ProgramaController::class,"save"]);
+    //  Equipe
+    Route::get("/equipe",[EquipeController::class,"index"]);
+    Route::get("/addequipe",[EquipeController::class,"edit"]);
+    Route::post("/addequipe",[EquipeController::class,"save"]);
+    //  Promoção
+    Route::get("/promocao",[PromocaoController::class,"index"]);
+    Route::get("/addpromocao",[PromocaoController::class,"edit"]);
+    Route::post("/addpromocao",[PromocaoController::class,"save"]);
+    
+    Route::get("/recados",[RecadoController,"index"]);
+    Route::get("/push",[PushController,"index"]);
+    Route::get("/config",[ConfigController,"index"]);
+    
 });
